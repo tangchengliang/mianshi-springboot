61391454d4291774969ae8f9afb7c74a86b046ea
Sy <2548446071@qq.com>
2025-02-25 10:33:22 +0800

    feat: redission配置

--- src/main/java/com/tcl/mianshi/MainApplication.java
+++ src/main/java/com/tcl/mianshi/MainApplication.java
@@ -13,7 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author tcl
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+//@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.tcl.mianshi.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
