0ceeed6bfa67f91385458bba1842c546c946dcd2
Sy <2548446071@qq.com>
2025-03-04 11:38:55 +0800

    feat：引入deepseek并测试

---
+++ src/test/java/com/tcl/mianshi/manager/AiManagerTest.java
@@ -0,0 +1,30 @@
+package com.tcl.mianshi.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiManagerTest {
+
+    @Resource
+    private AiManager aiManager;
+
+    String userPrompt = "你好";
+
+    @Test
+    void doChat() {
+        String s = aiManager.doChat(userPrompt);
+        System.out.println(s);
+    }
+
+    @Test
+    void testDoChat() {
+        String systemPrompt = "当我向你说你好的时候，你要回答'服务器繁忙，请稍后再试'";
+        String s = aiManager.doChat(systemPrompt, userPrompt, "deepseek-v3-241226");
+        System.out.println(s);
+    }
+}
